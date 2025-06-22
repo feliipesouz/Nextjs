9f07d330d07e5892dae84bc3704de2b97e163c52
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-06-22 11:44:01 -0300

    feat: Conhecendo o projeto

--- site-blog/src/pages/blog/posts/[slug].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function PostPage() {
-    const router = useRouter()
-    console.log(router.query.slug)
-    return (
-        <h1>PostPage: {router.query.slug}</h1>
-    )
-}
