dca3ec0da336c6314707ef70c2006dc8198b07a5
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-08-28 18:00:38 -0300

    feat: Finalizando o Search

--- site-blog/src/components/search/search.tsx
+++ site-blog/src/components/search/search.tsx
@@ -1,11 +1,14 @@
-import { useRouter } from "next/router";
-import { useCallback, useState } from "react";
-import { SearchIcon } from "lucide-react"
-import clsx from "clsx";
+import { cn } from '@/lib/utils';
+import { CircleX, SearchIcon } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback, useEffect, useRef } from 'react';
 
 export const Search = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const router = useRouter();
-  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const query = searchParams?.get('q') ?? '';
+  const hasQuery = !!searchParams?.has('q');
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -19,31 +22,45 @@ export const Search = () => {
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newQuery = event.target.value;
-    setQuery(newQuery);
-
-    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
-      shallow: true,
+    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, {
       scroll: false,
     });
   };
 
+  const resetSearch = () => {
+    router.push('/blog', { scroll: false });
+  };
+
+  useEffect(() => {
+    if (hasQuery) {
+      inputRef.current?.focus();
+    }
+  }, [hasQuery]);
+
   return (
-    <form onSubmit={handleSearch} className="relative group">
+    <form onSubmit={handleSearch} className="relative group w-full md:w-60">
       <SearchIcon
-        className={clsx(
-          "text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300",
-          query ? "text-blue-300" : ""
+        className={cn(
+          'text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300',
+          query ? ' text-blue-300' : ''
         )}
       />
 
       <input
+        ref={inputRef}
         type="text"
+        value={query}
         placeholder="Buscar"
         onChange={handleQueryChange}
-        className="h-10 w-72 bg-transparent border border-gray-400 pl-9 rounded-md text-body-sm text-gray-100 outline-none transition-all duration-200
-        focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300
-        placeholder:text-gray-300 placeholder:text-body-sm"
+        className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
       />
+
+      {query && (
+        <CircleX
+          className="absolute w-4 h-4 top-1/2 -translate-y-1/2 right-3 text-gray-300"
+          onClick={resetSearch}
+        />
+      )}
     </form>
   );
 };
