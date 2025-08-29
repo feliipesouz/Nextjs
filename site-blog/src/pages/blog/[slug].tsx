1812fb584a13193b537b5467afe0d44f376e32bb
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-08-29 16:47:39 -0300

    feat: Componente de Avatar

--- site-blog/src/pages/blog/[slug].tsx
+++ site-blog/src/pages/blog/[slug].tsx
@@ -1,48 +1,77 @@
-import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { allPosts } from "contentlayer/generated";
-import { Link } from "lucide-react";
+import { Post } from 'contentlayer/generated';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function PostPage() {
-  const router = useRouter()
-  const slug = router.query.slug as string
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Avatar } from '@/components/avatar';
 
-  const post = allPosts.find(post => post.slug.toLowerCase().includes(slug.toLowerCase()))
+
+export type PostPageProps = {
+  post: Post;
+};
+
+export const PostPage = ({ post }: PostPageProps) => {
+  const publishedDate = new Date(post?.date).toLocaleDateString('pt-BR');
 
   return (
-    <main className="mt-32 text-gray-100">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild className="text-action-sm">
-              <Link href="/blog">Blog</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
+    <main className="py-20 text-gray-100">
+      <div className="container space-y-8 px-4 md:px-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="text-action-sm">
+                <Link href="/blog">Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <span className="text-blue-200 text-action-sm">
+                {post?.title}
+              </span>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
 
-          <BreadcrumbSeparator />
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+          <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
+            <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+              <Image
+                src={post?.image ?? ''}
+                alt={post?.title ?? ''}
+                fill
+                className="object-cover"
+              />
+            </figure>
 
-          <BreadcrumbItem>
-            <span className="text-blue-200 text-action-sm">
-              {post?.title}
-            </span>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+            <header className="p-4 md:p-6 lg:p-12 pb-0 mt-8 md:mt-12">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+                {post?.title}
+              </h1>
 
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
-        <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
-          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
-            <Image
-              src={post?.image ?? ""}
-              alt={post?.title ?? ""}
-              fill
-              className="object-cover"
-            />
-          </figure>
-        </article>
+              <Avatar.Container>
+                <Avatar.Image
+                  src={post?.author.avatar}
+                  alt={post?.title}
+                  size="sm"
+                />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    Publicado em {''}
+                    <time dateTime={post?.date}>{publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
+          </article>
+        </div>
       </div>
     </main>
   );
-
-}
+};
