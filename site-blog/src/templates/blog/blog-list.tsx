6f300f8f1cee854234f631fad07269007b0d7f9f
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-08-29 12:17:52 -0300

    feat: Criando a funcionalidade de busca

--- site-blog/src/templates/blog/blog-list.tsx
+++ site-blog/src/templates/blog/blog-list.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card/post-card";
 import { PostGridCard } from "./components/post-grid-card";
 import { allPosts } from "contentlayer/generated";
-
+import { Inbox } from "lucide-react";
 
 export function BlogList() {
   const router = useRouter();
@@ -12,7 +12,12 @@ export function BlogList() {
     ? `Resultados de busca para "${query}"`
     : "Dicas e estratégias para impulsionar seu negócio";
 
-  const posts = allPosts
+  const postList = query
+    ? allPosts.filter((post) =>
+      post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
+    )
+    : allPosts;
+  const hasPosts = postList?.length > 0;
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -29,22 +34,34 @@ export function BlogList() {
           <Search />
         </div>
       </header>
-      <PostGridCard>
-        {posts.map((post) => (
-          <PostCard
-            key={post._id}
-            title={post.title}
-            description={post.description}
-            date={new Date(post.date).toLocaleDateString('pt-BR')}
-            slug={post.slug}
-            image={post.image}
-            author={{
-              avatar: post.author.avatar,
-              name: post.author.name,
-            }}
-          />
-        ))}
-      </PostGridCard>
+      {hasPosts && (
+        <PostGridCard>
+          {postList.map((post) => (
+            <PostCard
+              key={post._id}
+              title={post.title}
+              description={post.description}
+              date={new Date(post.date).toLocaleDateString('pt-BR')}
+              slug={post.slug}
+              image={post.image}
+              author={{
+                avatar: post.author.avatar,
+                name: post.author.name,
+              }}
+            />
+          ))}
+        </PostGridCard>
+      )}
+
+      {!hasPosts && (
+        <div className="container px-8">
+          <div className="flex flex-col items-center justify-center gap-8 border-dashed border-2 border-gray-300 p-8 md:p-12 rounded-lg">
+            <Inbox className="h-12 w-12 text-cyan-100" />
+
+            <p className="text-gray-100 text-center">Nenhum post encontrado.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
