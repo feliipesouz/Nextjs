cacedcd4755ae27714f142d2a904ab940ce82b14
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-09-24 18:55:11 -0300

    feat: Entendendo SSG e ISR na App Router

--- site-blog/src/app/blog/[slug]/page.tsx
+++ site-blog/src/app/blog/[slug]/page.tsx
@@ -1,22 +1,50 @@
-import { PostPage } from "@/templates/blog"
-import { allPosts } from "contentlayer/generated"
-import { notFound } from "next/navigation"
+import { PostPage } from '@/templates/blog';
+import { allPosts } from 'contentlayer/generated';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
-    slug: string
-  }>
-}
+    slug: string;
+  }>;
+};
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
-  const { slug } = await params
-  const post = allPosts.find((post) => post.slug === slug)
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
 
   if (!post) {
-    notFound()
+    return {};
   }
 
-  return (
-    <PostPage post={post} />
-  )
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return <PostPage post={post} />;
 }
